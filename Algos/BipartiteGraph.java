93c38b4da93c7d347f4a9bf53b3aa162dce0e8e8
Aaron Liu <zlsuperdan@gmail.com>
2018-05-17 00:24:41 -0700

    algo changes

--- Algos/BipartiteGraph.java
+++ Algos/BipartiteGraph.java
@@ -1,9 +1,18 @@
 /*
-带权二分图的最大匹配
+带权二分图的最大匹配 -> KM算法
+
+步骤：
+（1）初始化可行标杆
+（2）用匈牙利算法寻找完备匹配
+（3）若未找到完备匹配则修改可行标杆
+（4）重复（2）（3）直到找到相等子图的完备匹配
 
 
 
 
 
 
+reference: 
+https://blog.csdn.net/x_y_q_/article/details/51927054
+http://www.cnblogs.com/liuzhen1995/p/6740967.html
 */
