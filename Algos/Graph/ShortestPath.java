aa2769a950ef65277a16d93dd21473f90a963049
Aaron Liu <zlsuperdan@gmail.com>
2018-06-14 00:44:19 -0700

    update dijkstra algo

--- Algos/Graph/ShortestPath.java
+++ Algos/Graph/ShortestPath.java
@@ -1,7 +1,29 @@
+/*
+最短路算法
+1.Dijkstra
+原理是贪心算法 保证每一步都是最短
+http://www.wayne.ink/2018/02/06/Algorithm/Dijkstra/
+http://threezj.com/2016/05/02/%E6%9C%80%E7%9F%AD%E8%B7%AF%E5%BE%84%E7%AE%97%E6%B3%95%E6%80%BB%E7%BB%93/
+https://blog.csdn.net/v_JULY_v/article/details/6096981
+
+2.Bellman-Ford
+
+
+3.Floyd-Warshall
+解决任意两点间的最短路径的一种算法 可以正确处理有向图或负权的最短路径问题
+
+
+几个最短路径算法的比较
+https://blog.csdn.net/v_july_v/article/details/6181485
+ */
 
 import java.util.*;
 
 public class ShortestPath {
-  private Graph graph;
-  
+
+  private int[] prev
+
+
+
+
 }
